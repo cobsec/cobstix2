c11f26e63f3d78492bcca36b82320838380a9f0f
cob <c_j_obrien@hotmail.com>
2017-02-15 16:44:31 +0000

    Some changes to ingest.py to make it a bit more useful. Demo's the use of a stix bundle to interpret context of a delivered Indicator object (and linked objects).

--- ingest.py
+++ ingest.py
@@ -6,6 +6,18 @@ def main():
 
   obj = file_to_obj('out.json')
   print obj
+  if obj.type == 'bundle':
+    output = {'pattern': [], 'coa': [], 'context': []}
+    for _dict in obj.objects:
+      object = dict_to_obj(_dict)
+      if object.type == 'indicator':
+        output['pattern'].append(object.pattern)
+      if object.type == 'course-of-action':
+        output['coa'].append(object.name)
+      if object.type == 'attack-pattern':
+        output['context'].append(object.name)
+
+  print output
 
 if __name__ == '__main__':
   main()
